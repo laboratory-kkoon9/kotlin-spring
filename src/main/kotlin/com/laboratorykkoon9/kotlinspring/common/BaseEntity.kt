4ddab51b628624aa0cf0975aef1e4acd766678dc
kkoon9 <rndrnjs2003@gmail.com>
2023-08-10 18:22:48 +0900

    feat: BaseEntity 적용
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/kotlin/com/laboratorykkoon9/kotlinspring/common/BaseEntity.kt
+++ src/main/kotlin/com/laboratorykkoon9/kotlinspring/common/BaseEntity.kt
@@ -1,4 +1,16 @@
 package com.laboratorykkoon9.kotlinspring.common
 
-class BaseEntity {
+import jakarta.persistence.Column
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
+
+open class BaseEntity {
+    @Column(name = "created_at")
+    @CreatedDate
+    val createdAt: LocalDateTime? = LocalDateTime.now()
+
+    @Column(name = "updated_at")
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = null
 }

deb74cedf31b27b53f65a9565cff35d86a7e01ec
kkoon9 <rndrnjs2003@gmail.com>
2023-08-10 21:05:20 +0900

    feat: querydsl JPAQueryFactory bug fix를 위한 config

--- src/main/kotlin/com/laboratorykkoon9/kotlinspring/config/JpaConfig.kt
+++ src/main/kotlin/com/laboratorykkoon9/kotlinspring/config/JpaConfig.kt
@@ -1,8 +1,22 @@
 package com.laboratorykkoon9.kotlinspring.config
 
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @Configuration
 @EnableJpaAuditing
 class JpaConfig
+
+@Configuration
+class QuerydslConfig(
+    @PersistenceContext private val entityManager: EntityManager
+) {
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(this.entityManager)
+    }
+}

022c8f6da2d181e2309fc3c6ef4f59ef81026c41
kkoon9 <rndrnjs2003@gmail.com>
2023-08-10 18:22:32 +0900

    chore: jpa EnableJpaAuditing 설정 추가
    Signed-off-by: kkoon9 <rndrnjs2003@gmail.com>

--- src/main/kotlin/com/laboratorykkoon9/kotlinspring/config/JpaConfig.kt
+++ src/main/kotlin/com/laboratorykkoon9/kotlinspring/config/JpaConfig.kt
@@ -1,4 +1,8 @@
 package com.laboratorykkoon9.kotlinspring.config
 
-class JpaConfig {
-}
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig
